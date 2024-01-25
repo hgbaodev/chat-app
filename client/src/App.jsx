df0a0e1907c829a3334c7bd149e819d16fdb52cd
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-01-25 17:09:46 +0700

    update config client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
-import { Button } from 'antd';
+import { useId } from "react";
 
 function App() {
+  const vegetableSelectId = useId();
   return (
-    <div>
-      <h1 className="underline text-blue-600">
-      Hello world!
-    </h1>
-    <Button type="primary">Button</Button>
-    </div>
-  )
+    <>
+      <h1 className="text-red-100 text-center p-10 bg-slate-600 rounded-md">hgbaodev</h1>
+      <label>
+        Pick a fruit:
+        <select name="selectedFruit">
+          <option value="apple">Apple</option>
+          <option value="banana">Banana</option>
+          <option value="orange">Orange</option>
+        </select>
+      </label>
+      <hr />
+      <label htmlFor={vegetableSelectId}>Pick a vegetable:</label>
+      <select id={vegetableSelectId} name="selectedVegetable">
+        <option value="cucumber">Cucumber</option>
+        <option value="corn">Corn</option>
+        <option value="tomato">Tomato</option>
+      </select>
+    </>
+  );
 }
 
-export default App
+export default App;
