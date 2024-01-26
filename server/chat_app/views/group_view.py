f322165ba5f54e4253610d00fa4683633357a79d
lequanphat <lequanphat3579@gmail.com>
2024-01-26 10:14:28 +0700

    group apis

---
+++ server/chat_app/views/group_view.py
@@ -0,0 +1,23 @@
+from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
+from django.http import JsonResponse
+from rest_framework import status
+
+from chat_app.serializers import GroupSerializer
+from chat_app.models import Group
+
+class CreateGroup(CreateAPIView):
+
+    def post(self, request, *args, **kwargs):
+        serializer = GroupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GetAllGroups(APIView):
+    def get(self, request, user_id):
+        groups = Group.objects.filter(createdBy=user_id)
+        serializer = GroupSerializer(groups, many=True)
+        # response
+        return JsonResponse(serializer.data, safe=False, status=status.HTTP_200_OK)
