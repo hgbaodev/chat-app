e41050e023e5958d91798100305654c6ad0cf59e
lequanphat <lequanphat3579@gmail.com>
2024-01-28 13:01:34 +0700

    chat page

--- client/src/components/Loader.jsx
+++ client/src/components/Loader.jsx
@@ -1,12 +1,7 @@
 import { Flex, Spin } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 const Loader = () => (
-  <Flex
-    align="center"
-    justify="center"
-    className="fixed top-0 left-0 right-0 bottom-0 z-[999] "
-    style={{ background: 'rgba(0, 0, 0, 0.2)' }}
-  >
+  <Flex align="center" justify="center" className="fixed top-0 left-0 right-0 bottom-0 z-[999] bg-opacity-20 bg-black">
     <Spin indicator={<LoadingOutlined style={{ fontSize: 36 }} spin />} />
   </Flex>
 );
--- client/src/pages/dashboard/Chat.jsx
+++ client/src/pages/dashboard/Chat.jsx
@@ -1,4 +1,17 @@
+import { Col, Row } from 'antd';
+import { ChatContainer } from '~/section/chats/ChatContainer';
+import { Contacts } from '~/section/chats/Contacts';
+
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <Row className="h-full ">
+      <Col span={5} className="p-4" style={{ boxShadow: '0px 0px 2px rgba(0,0,0,.2)' }}>
+        <Contacts />
+      </Col>
+      <Col span={19}>
+        <ChatContainer />
+      </Col>
+    </Row>
+  );
 };
 export default Chat;
