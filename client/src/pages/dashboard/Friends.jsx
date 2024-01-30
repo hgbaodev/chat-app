6c429b598d16b7f5fe86e9c0d06a7baae30de6fb
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-01-30 09:01:02 +0700

    style contact item

---
+++ client/src/pages/dashboard/Friends.jsx
@@ -0,0 +1,10 @@
+
+const Friends = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, eveniet?
+    </div>
+  );
+};
+
+export default Friends;
--- client/src/section/chats/ContactItem.jsx
+++ client/src/section/chats/ContactItem.jsx
@@ -1,24 +1,30 @@
-import { Avatar, Flex, Space, Typography } from 'antd';
-import { faker } from '@faker-js/faker';
+import { Avatar, Flex, Space, Typography } from "antd";
+import { faker } from "@faker-js/faker";
 export const ContactItem = () => {
   return (
-    <Flex className="bg-gray-100 p-2 rounded-md cursor-pointer" align="center" justify="space-between">
+    <Flex
+      className="bg-gray-100 p-2 rounded-md cursor-pointer"
+      align="center"
+      justify="space-between"
+    >
       <Space>
-        <Avatar style={{ backgroundColor: '#fde3cf', color: '#f56a00' }}>
-          {' '}
+        <Avatar style={{ backgroundColor: "#fde3cf", color: "#f56a00" }}>
+          {" "}
           {faker.person.fullName()[0].toUpperCase()}
         </Avatar>
         <Flex vertical justify="center">
           <Typography className="font-bold overflow-hidden whitespace-nowrap text-ellipsis max-w-[120px]">
             {faker.person.fullName()}
           </Typography>
-          <Typography className="overflow-hidden whitespace-nowrap text-ellipsis max-w-[120px]">
+          <Typography className="text-xs text-neutral-500 overflow-hidden whitespace-nowrap text-ellipsis max-w-[120px]">
             {faker.lorem.sentence()}
           </Typography>
         </Flex>
       </Space>
-      <Typography>
-        {faker.date.anytime().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}
+      <Typography className="text-[10px] text-neutral-500">
+        {faker.date
+          .anytime()
+          .toLocaleTimeString("en-US", { hour: "numeric", minute: "numeric" })}
       </Typography>
     </Flex>
   );
