51dbc9b1fe45f6acb26aca6f2b45d28759a84f24
lequanphat <lequanphat3579@gmail.com>
2024-01-31 11:59:20 +0700

    friend-request-item

--- client/src/section/contacts/FriendRequestItem.jsx
+++ client/src/section/contacts/FriendRequestItem.jsx
@@ -1,13 +1,15 @@
 import { Avatar, Button, Flex } from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import { IoCloseOutline } from 'react-icons/io5';
 import { faker } from '@faker-js/faker';
 
 export const FriendRequestItem = () => {
   return (
-    <div className="bg-blue-100 p-4 rounded-md">
+    <div className="bg-white p-4 rounded-md" style={{ boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px' }}>
       <Flex align="center" justify="space-between">
         <Flex align="center" gap={10}>
-          <Avatar className="bg-[#fde3cf] text-[#f56a00]">{faker.person.fullName()[0].toUpperCase()}</Avatar>
+          <Avatar size={40} className="bg-[#fde3cf] text-[#f56a00]">
+            {faker.person.fullName()[0].toUpperCase()}
+          </Avatar>
           <div>
             <p className="my-1 font-semibold">{faker.person.firstName()}</p>
             <p className="m-0 text-[12px]">
@@ -15,7 +17,14 @@ export const FriendRequestItem = () => {
             </p>
           </div>
         </Flex>
-        <Button type="text" shape="circle" icon={<SearchOutlined />} />
+        <Button type="text" shape="circle" icon={<IoCloseOutline size={20} />} />
+      </Flex>
+      <div className="h-[60px] p-2  bg-gray-50 my-4 rounded border-[1px] border-gray-300 border-solid">
+        <p className="m-0 line-clamp-2 text-ellipsis overflow-hidden">{faker.lorem.sentence()}</p>
+      </div>
+      <Flex align="center" justify="space-between">
+        <Button className="w-[48%]">CANCEL</Button>
+        <Button type='primary' className="w-[48%]">ACCEPT</Button>
       </Flex>
     </div>
   );
--- client/src/section/contacts/FriendRequests.jsx
+++ client/src/section/contacts/FriendRequests.jsx
@@ -7,7 +7,7 @@ const FriendRequests = () => {
       <Flex align="center" className="h-[53px] p-4" style={{ boxShadow: '0px 1px 1px rgba(0,0,0,.1)' }}>
         <p className="font-semibold">Friend Requests (20)</p>
       </Flex>
-      <Flex vertical className="p-4 " gap={10}>
+      <Flex vertical className="p-4 bg-gray-100" gap={10}>
         <p className="font-semibold">Lời mời đã nhận (5)</p>
         <Flex justify="start" gap={14}>
           <Row gutter={[16, 16]}>
