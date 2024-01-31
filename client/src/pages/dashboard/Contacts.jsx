2e4d98fc81526128430c24b735f3c0547941d7d6
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-01-31 08:26:42 +0700

    Refactor dashboard Contacts component and TabItem
    Refactored the Contacts component and TabItem in dashboard to use
    updated UI components and improve layout and functionality. Introduced
    new search and sorting features for the friend list. This improves the
    user experience and makes the dashboard more interactive and
    user-friendly. No associated issues.

--- client/src/pages/dashboard/Contacts.jsx
+++ client/src/pages/dashboard/Contacts.jsx
@@ -1,48 +1,51 @@
-import { Avatar, Col, Flex, List, Row, Typography } from 'antd';
-import Title from 'antd/es/typography/Title';
-import { IoPeopleOutline, IoPersonAddOutline, IoPersonOutline } from 'react-icons/io5';
-
-const data = [
-  {
-    title: 'Ant Design Title 1',
-  },
-  {
-    title: 'Ant Design Title 2',
-  },
-  {
-    title: 'Ant Design Title 3',
-  },
-  {
-    title: 'Ant Design Title 4',
-  },
-];
+import { Col, Flex, Input, Row, Select, Typography } from "antd";
+import {
+  IoPeopleOutline,
+  IoPersonAddOutline,
+  IoPersonOutline,
+  IoSearchOutline,
+} from "react-icons/io5";
+import { FriendItem } from "~/section/friends/FriendItem";
 
 const Contacts = () => {
   return (
     <Row className="h-screen">
-      <Col span={5} style={{ boxShadow: '0px 0px 2px rgba(0,0,0,.2)' }}>
-        <TabItem text="Friend Lists" icon={<IoPersonOutline size={22} />} active />
-        <TabItem text="Joined Groups" icon={<IoPeopleOutline size={24} />} />
-        <TabItem text="Friend Requests" icon={<IoPersonAddOutline size={22} />} />
-      </Col>
-      <Col span={19} >
-        <Flex align='center' className="h-[60px] p-4" style={{ boxShadow: '0px 0px 2px rgba(0,0,0,.2)' }}>
-          <p className='font-semibold'>Friends (20)</p>
-        </Flex>
-        <List
-          className="w-[100%] p-4"
-          itemLayout="horizontal"
-          dataSource={data}
-          renderItem={(item, index) => (
-            <List.Item>
-              <List.Item.Meta
-                avatar={<Avatar size="large" src={`https://api.dicebear.com/7.x/miniavs/svg?seed=${index}`} />}
-                title={<a href="https://ant.design">{item.title}</a>}
-                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-              />
-            </List.Item>
-          )}
+      <Col span={5} style={{ boxShadow: "0px 0px 2px rgba(0,0,0,.2)" }}>
+        <TabItem
+          text="Friend Lists"
+          icon={<IoPersonOutline size={22} />}
+          active
         />
+        <TabItem text="Joined Groups" icon={<IoPeopleOutline size={24} />} />
+        <TabItem
+          text="Friend Requests"
+          icon={<IoPersonAddOutline size={22} />}
+        />
+      </Col>
+      <Col span={19}>
+        <Flex
+          align="center"
+          className="h-[60px] p-4"
+          style={{ boxShadow: "0px 0px 2px rgba(0,0,0,.2)" }}
+        >
+          <p className="font-semibold">Friends (20)</p>
+        </Flex>
+        <Flex className="p-4" gap={10}>
+          <Input
+            placeholder="Search friends"
+            prefix={<IoSearchOutline />}
+            className="w-[350px]"
+          />
+          <Select
+            className="w-[250px]"
+            defaultValue="asc"
+            options={[
+              { value: "asc", label: "Name (A-Z)" },
+              { value: "desc", label: "Name (Z-A)" },
+            ]}
+          />
+        </Flex>
+        <FriendItem />
       </Col>
     </Row>
   );
@@ -50,7 +53,13 @@ const Contacts = () => {
 
 const TabItem = ({ text, icon, active }) => {
   return (
-    <Flex className={`${active ? 'bg-blue-50 ' : ''} p-4 cursor-pointer hover:bg-gray-100`} gap={20} align="center">
+    <Flex
+      className={`${
+        active ? "bg-blue-50 " : ""
+      } p-4 cursor-pointer hover:bg-gray-100`}
+      gap={20}
+      align="center"
+    >
       {icon}
       <Typography className="font-bold">{text}</Typography>
     </Flex>
