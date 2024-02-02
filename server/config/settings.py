beb391fd3264550133ca2d71875de801c2efa713
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-02-02 23:23:05 +0700

    update docker

--- server/config/settings.py
+++ server/config/settings.py
@@ -97,8 +97,12 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "chat_app",
         "USER": "root",
-        "PASSWORD": "",
+        "PASSWORD": "pass",
         "HOST": "127.0.0.1",
+        "PORT": '3307',
+        "OPTIONS": {
+          'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
