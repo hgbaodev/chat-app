10214afcb2fe1788fa4b3b2341a4c7ed41e477bb
lequanphat <lequanphat3579@gmail.com>
2024-02-03 19:27:25 +0700

    shared message - docs

--- client/src/section/chats/ChatFooter.jsx
+++ client/src/section/chats/ChatFooter.jsx
@@ -4,7 +4,14 @@ import { useState } from 'react';
 import { IoIosLink } from 'react-icons/io';
 import { IoHappyOutline, IoSendSharp } from 'react-icons/io5';
 export const ChatFooter = () => {
+  const [text, setText] = useState('');
   const [isOpenEmojiPicker, setOpenEmojiPicker] = useState(false);
+  // handle
+  const handleEmojiClick = (emoji) => {
+    setText((pre) => pre + emoji.emoji);
+  };
+
+  // render
   return (
     <Flex className="relative h-[60px] p-3" align="center" gap="small">
       <Button
@@ -19,18 +26,22 @@ export const ChatFooter = () => {
         shape="circle"
         icon={<IoHappyOutline size={24} />}
         size="large"
-        className="text-blue-500 hover:bg-blue-700"
+        className=" text-blue-500 hover:bg-blue-700"
         onClick={() => {
           setOpenEmojiPicker(!isOpenEmojiPicker);
         }}
       />
       {isOpenEmojiPicker && (
         <div className="absolute bottom-[60px] left-[60px] ">
-          <EmojiPicker />
+          <EmojiPicker onEmojiClick={handleEmojiClick} />
         </div>
       )}
 
       <Input
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
         placeholder="Message..."
         className="h-full rounded-full bg-blue-50 border-none focus:shadow-none hover:bg-blue-100 focus:bg-blue-100"
       />
