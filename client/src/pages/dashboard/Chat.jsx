5ee5404662eca43bc42be0fb37e5e32364e9474b
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-02-27 17:24:12 +0700

    Revert "update ui register"
    This reverts commit 582a7e41ff16ca21f5c3bf8c8f1856f2026e3250.

--- client/src/pages/dashboard/Chat.jsx
+++ client/src/pages/dashboard/Chat.jsx
@@ -28,12 +28,12 @@ const Chat = () => {
         {contactInfo.open &&
           (() => {
             switch (contactInfo.type) {
-            case 'CONTACT':
-              return <ContactInfo />;
-            case 'SHARED':
-              return <SharedMessages />;
-            default:
-              return <></>;
+              case 'CONTACT':
+                return <ContactInfo />;
+              case 'SHARED':
+                return <SharedMessages />;
+              default:
+                return <></>;
             }
           })()}
       </Flex>
--- client/src/routes/AuthRoutes.jsx
+++ client/src/routes/AuthRoutes.jsx
@@ -1,6 +1,5 @@
 import { lazy } from 'react';
 import Loadable from '~/components/Loadable';
-import AuthLayout from '~/layouts/AuthLayout';
 
 const Login = Loadable(lazy(() => import('~/pages/auth/Login')));
 const Register = Loadable(lazy(() => import('~/pages/auth/Register')));
@@ -11,18 +10,12 @@ const AuthRoutes = {
   errorElement: <Page404 />,
   children: [
     {
-      path: '/auth',
-      element: <AuthLayout />,
-      children: [
-        {
-          path: 'login',
-          element: <Login />
-        },
-        {
-          path: 'register',
-          element: <Register />
-        }
-      ]
+      path: 'login',
+      element: <Login />
+    },
+    {
+      path: 'register',
+      element: <Register />
     }
   ]
 };
