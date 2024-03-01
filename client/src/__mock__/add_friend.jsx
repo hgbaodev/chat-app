61dd38447472e8624f8d1f9db4da77224c31a6a4
lequanphat <lequanphat3579@gmail.com>
2024-03-01 21:09:37 +0700

    mock data add-friend-modal

---
+++ client/src/__mock__/add_friend.jsx
@@ -0,0 +1,87 @@
+import { faker } from '@faker-js/faker';
+export const __add_friend_mock__ = [
+  {
+    id: 0,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 0
+  },
+  {
+    id: 1,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 0
+  },
+  {
+    id: 2,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 0
+  },
+  {
+    id: 3,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 1
+  },
+  {
+    id: -1,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 2
+  },
+  {
+    id: 4,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 0
+  },
+  {
+    id: 5,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 1
+  },
+  {
+    id: 6,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 0
+  },
+  {
+    id: 7,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 0
+  },
+  {
+    id: 8,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 1
+  },
+  {
+    id: 9,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 0
+  },
+  {
+    id: 10,
+    avatarSr: faker.image.avatarLegacy(),
+    fullName: faker.person.fullName(),
+    email: faker.internet.email(),
+    status: 1
+  }
+];
--- client/src/section/common/AddFriendsModal.jsx
+++ client/src/section/common/AddFriendsModal.jsx
@@ -1,11 +1,12 @@
 import { Avatar, Button, Flex, Input, Modal, Space } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
-import { faker } from '@faker-js/faker';
 import useHover from '~/hooks/useHover';
-import { useMemo } from 'react';
-import { IoCloseOutline } from 'react-icons/io5';
+import { useState } from 'react';
+import { IoAdd } from 'react-icons/io5';
+import { __add_friend_mock__ } from '~/__mock__/add_friend';
 
 const AddFriendsModal = ({ isModalOpen, setIsModalOpen }) => {
+  const [search, setSearch] = useState('');
   const handleClose = () => {
     setIsModalOpen(false);
   };
@@ -13,9 +14,9 @@ const AddFriendsModal = ({ isModalOpen, setIsModalOpen }) => {
     <Modal
       title="Add friend"
       open={isModalOpen}
-      onOk={handleClose}
       onCancel={handleClose}
       width={400}
+      footer={null}
     >
       <Space direction="vertical" className="w-[100%]">
         <Input
@@ -25,30 +26,58 @@ const AddFriendsModal = ({ isModalOpen, setIsModalOpen }) => {
           prefix={<SearchOutlined />}
           className="mt-2"
           autoComplete="nope"
+          value={search}
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
         />
-        <p className="text-xs text-gray-500">Recent searches</p>
-        <div>
-          <UserSearchItem />
-          <UserSearchItem />
-          <UserSearchItem />
-          <UserSearchItem />
-          <UserSearchItem />
-          <UserSearchItem />
+        {search ? (
+          <p className="text-xs text-gray-500">Recent searches</p>
+        ) : (
+          <p className="text-xs text-gray-500">Friendship suggestions</p>
+        )}
+        <div className="max-h-[420px] overflow-y-auto scrollbar">
+          {__add_friend_mock__.map((item) => (
+            <UserSearchItem
+              key={item.id}
+              avatarSrc={item.avatarSr}
+              fullName={item.fullName}
+              email={item.email}
+              status={item.status}
+            />
+          ))}
         </div>
       </Space>
     </Modal>
   );
 };
 
-const UserSearchItem = () => {
+const UserSearchItem = ({ avatarSrc, fullName, email, status }) => {
   const [hoverRef, isHovering] = useHover();
-  const avatarSrc = useMemo(() => faker.image.avatarLegacy(), []);
-  const fullName = useMemo(() => faker.person.fullName(), []);
-  const email = useMemo(() => faker.internet.email(), []);
+  // handle
+  const renderButton = () => {
+    switch (status) {
+      case 0:
+        return (
+          <Button
+            type="primary"
+            size="small"
+            shape="circle"
+            icon={<IoAdd size={22} />}
+          />
+        );
+      case 1:
+        return <p className="m-0">Friend</p>;
+      case 2:
+        return <p className="m-0">Pending</p>;
+      default:
+        return 0;
+    }
+  };
   return (
     <Flex
       ref={hoverRef}
-      className="py-2 cursor-pointer"
+      className={`py-2 cursor-pointer rounded ${isHovering && 'bg-gray-100'}`}
       align="center"
       justify="space-between"
     >
@@ -59,13 +88,7 @@ const UserSearchItem = () => {
           <p className="m-0 text-xs text-gray-500">{email}</p>
         </Space>
       </Space>
-      {isHovering && (
-        <Button
-          type="text"
-          shape="circle"
-          icon={<IoCloseOutline size={22} />}
-        />
-      )}
+      {renderButton()}
     </Flex>
   );
 };
