11959d6f67983e60ce759236406bdf37fd4a489e
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-03-09 15:39:36 +0700

    Update views.py

--- server/authentication/views.py
+++ server/authentication/views.py
@@ -25,7 +25,7 @@ class RegisterUserView(GenericAPIView):
                 'data': user,
                 'message': 'User created successfully'
             }, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class LoginUserView(GenericAPIView):
     serializer_class = LoginSerializer
