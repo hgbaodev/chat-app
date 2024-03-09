56ae773970a9c8e376be8d571687e9e7e2cb3f40
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-03-09 16:01:36 +0700

    remove toast-message

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,6 @@ import App from './App';
 import reportWebVitals from '~/reportWebVitals';
 import { Provider as ReduxProvider } from 'react-redux';
 import { store } from './store';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -12,7 +10,6 @@ const root = createRoot(container);
 root.render(
   <ReduxProvider store={store}>
     <App />
-    <ToastContainer autoClose={1500} pauseOnHover={false} />
   </ReduxProvider>
 );
 
--- client/src/section/auth/FormLogin.jsx
+++ client/src/section/auth/FormLogin.jsx
@@ -1,93 +1,100 @@
-import { Button, Checkbox, Form, Input, Typography } from 'antd';
+import { Button, Checkbox, Form, Input, Typography, message } from 'antd';
 
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from '~/store';
 import { login } from '~/store/slices/authSlice';
-import { toast } from 'react-toastify';
 
 const { Text } = Typography;
 
 const FormLogin = () => {
   const navigate = useNavigate();
   const [form] = Form.useForm();
+  const [messageApi, contextHolder] = message.useMessage();
   const dispatch = useDispatch();
   const { isLoadingLogin } = useSelector((state) => state.auth);
   const onFinish = async (values) => {
     const response = await dispatch(login(values));
     if (response.error && response.payload) {
-      toast.error(response.payload?.detail);
+      messageApi.open({
+        type: 'error',
+        content: response.payload?.detail
+      });
+      console.log('Error login');
     } else {
       navigate('/');
     }
   };
   return (
-    <Form
-      form={form}
-      layout="vertical"
-      className="w-full"
-      initialValues={{ remember: true }}
-      requiredMark="optional"
-      onFinish={onFinish}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        validateTrigger="onBlur"
-        rules={[
-          { required: true, message: 'Please input your username!' },
-          { type: 'email', message: 'Email is not valid' }
-        ]}
+    <>
+      {contextHolder}
+      <Form
+        form={form}
+        layout="vertical"
+        className="w-full"
+        initialValues={{ remember: true }}
+        requiredMark="optional"
+        onFinish={onFinish}
       >
-        <Input placeholder="Email address" variant="filled" size="large" />
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        name="password"
-        validateTrigger="onBlur"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!'
-          },
-          {
-            min: 6,
-            message: 'Please password min 6 characters'
-          }
-        ]}
-      >
-        <Input.Password
-          type="password"
-          placeholder="Password"
-          variant="filled"
-          size="large"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
+        <Form.Item
+          label="Email"
+          name="email"
+          validateTrigger="onBlur"
+          rules={[
+            { required: true, message: 'Please input your username!' },
+            { type: 'email', message: 'Email is not valid' }
+          ]}
+        >
+          <Input placeholder="Email address" variant="filled" size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          validateTrigger="onBlur"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your password!'
+            },
+            {
+              min: 6,
+              message: 'Please password min 6 characters'
+            }
+          ]}
+        >
+          <Input.Password
+            type="password"
+            placeholder="Password"
+            variant="filled"
+            size="large"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Form.Item name="remember" valuePropName="checked" noStyle>
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Link className="float-right" href="/auth/forgot-password">
+            Forgot password
+          </Link>
         </Form.Item>
 
-        <Link className="float-right" href="/auth/forgot-password">
-          Forgot password
-        </Link>
-      </Form.Item>
-
-      <Form.Item>
-        <Button
-          type="primary"
-          htmlType="submit"
-          className="w-full"
-          size="large"
-          loading={isLoadingLogin}
-        >
-          Login
-        </Button>
-      </Form.Item>
-      <Form.Item className="text-center">
-        <Text>{"Don't have an account?"}</Text>
-        <Link to="/auth/register"> Sign up now</Link>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="w-full"
+            size="large"
+            loading={isLoadingLogin}
+          >
+            Login
+          </Button>
+        </Form.Item>
+        <Form.Item className="text-center">
+          <Text>{"Don't have an account?"}</Text>
+          <Link to="/auth/register"> Sign up now</Link>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
