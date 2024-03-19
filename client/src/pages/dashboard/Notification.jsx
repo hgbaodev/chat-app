b0fe6ab23e59ec3f66be9abb4a4fb657c9e1832d
lequanphat <lequanphat3579@gmail.com>
2024-03-19 20:56:44 +0700

    notification drawer

--- client/src/pages/dashboard/Notification.jsx
+++ client/src/pages/dashboard/Notification.jsx
@@ -1,45 +1,46 @@
-import { Flex, Space } from 'antd';
+import { Drawer, Space } from 'antd';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CustomLoader from '~/components/CustomLoader';
 import { NotificationItem } from '~/section/notifications/NotificationItem';
 import { getAllNotifications } from '~/store/slices/notificationSlice';
 
-const Notification = () => {
+const Notification = ({ open, handleClose }) => {
   const dispatch = useDispatch();
   const { notifications, isLoading } = useSelector(
     (state) => state.notifications
   );
   useEffect(() => {
-    dispatch(getAllNotifications());
-  }, [dispatch]);
+    if (open) {
+      dispatch(getAllNotifications());
+    }
+  }, [dispatch, open]);
 
   return (
-    <Flex className="h-full">
-      <Flex
-        vertical
-        className="w-[350px] p-4 cursor-pointer"
-        style={{ boxShadow: '0px 0px 2px rgba(0,0,0,.2)' }}
-      >
-        <h1 className="text-[16px] mb-4">Notifications</h1>
-        <Space direction="vertical" className="w-full">
-          {isLoading ? (
-            <CustomLoader />
-          ) : (
-            notifications.map((notification) => (
-              <NotificationItem
-                key={notification.id}
-                title={notification.title}
-                message={notification.message}
-                created_at={notification.created_at}
-                seen={notification.seen}
-              />
-            ))
-          )}
-        </Space>
-      </Flex>
-      <Flex></Flex>
-    </Flex>
+    <Drawer
+      title="Notifications"
+      placement={'left'}
+      closable={false}
+      onClose={handleClose}
+      open={open}
+      key={'left'}
+    >
+      <Space direction="vertical" className="w-full">
+        {isLoading ? (
+          <CustomLoader />
+        ) : (
+          notifications.map((notification) => (
+            <NotificationItem
+              key={notification.id}
+              title={notification.title}
+              message={notification.message}
+              created_at={notification.created_at}
+              seen={notification.seen}
+            />
+          ))
+        )}
+      </Space>
+    </Drawer>
   );
 };
 
