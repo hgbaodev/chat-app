57017b4f5772519444aff2dade945138937dd430
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-03-23 20:17:19 +0700

    Show members of group

--- client/src/section/chats/ChatHeader.jsx
+++ client/src/section/chats/ChatHeader.jsx
@@ -6,26 +6,43 @@ import {
 } from '@ant-design/icons';
 import { IoVideocamOutline } from 'react-icons/io5';
 import { useDispatch, useSelector } from 'react-redux';
-import { toggleContactInfo } from '~/store/slices/appSlice';
+import { showMembersGroup, toggleContactInfo } from '~/store/slices/appSlice';
+import { ConversationTypes } from '~/utils/enum';
+
 export const ChatHeader = () => {
   const dispatch = useDispatch();
   const { contactInfo } = useSelector((state) => state.app);
-  const { chat } = useSelector((state) => state.chat);
-  // handle
+  const { currentConversation } = useSelector((state) => state.chat.chat);
+
+  console.log(currentConversation);
+
   const handleOpenContactInfo = () => {
     dispatch(toggleContactInfo());
   };
+
+  const handleOpenSharedMessages = () => dispatch(showMembersGroup());
+
+  const getConversationInfo = () => {
+    if (currentConversation.type == ConversationTypes.GROUP) {
+      return (
+        <span className="cursor-pointer" onClick={handleOpenSharedMessages}>
+          {currentConversation.members.length} members
+        </span>
+      );
+    } else return <span>Online</span>;
+  };
+
   return (
     <Flex className="h-[60px] px-4" justify="space-between">
       <Space size="middle">
         <Badge size="default" dot={true} color="green" offset={[0, 28]}>
-          <Avatar src={chat.currentConversation.image} size="large" />
+          <Avatar src={currentConversation.image} size="large" />
         </Badge>
         <Flex vertical justify="center">
           <Typography className="font-bold">
-            {chat.currentConversation.title}
+            {currentConversation.title}
           </Typography>
-          <Typography className="text-[12px]">Online</Typography>
+          <Typography className="text-xs">{getConversationInfo()}</Typography>
         </Flex>
       </Space>
       <Space size={18}>
---
+++ client/src/section/chats/ListMemberOfGroup.jsx
@@ -0,0 +1,74 @@
+import { Avatar, Button, Flex, Space } from 'antd';
+import { IoArrowBackSharp } from 'react-icons/io5';
+import { useDispatch, useSelector } from '~/store';
+import { showContactInfo } from '~/store/slices/appSlice';
+import { LuUserPlus } from 'react-icons/lu';
+import { FiUserPlus } from 'react-icons/fi';
+
+const ListMemberOfGroup = () => {
+  const dispatch = useDispatch();
+  const { currentConversation } = useSelector((state) => state.chat.chat);
+
+  // handle
+  const handleReturnContactInfo = () => {
+    dispatch(showContactInfo());
+  };
+
+  // render
+  return (
+    <Flex
+      vertical
+      className="w-[350px]"
+      style={{ boxShadow: '0px 0px 2px rgba(0,0,0,.1)' }}
+    >
+      <Flex
+        gap={10}
+        align="center"
+        className="w-full h-[60px] px-4"
+        style={{ boxShadow: '0px 0px 2px rgba(0,0,0,.1)' }}
+      >
+        <Button
+          type="text"
+          shape="circle"
+          icon={<IoArrowBackSharp size={18} />}
+          size={20}
+          onClick={handleReturnContactInfo}
+        />
+        <p className="m-0 font-semibold">Members</p>
+      </Flex>
+
+      <Space className="p-4" direction="vertical">
+        <Button type="primary" className="w-full" icon={<LuUserPlus />}>
+          Add members
+        </Button>
+        <Flex vertical>
+          {currentConversation.members.map((member) => (
+            <MemberItem key={member.id} {...member} />
+          ))}
+        </Flex>
+      </Space>
+    </Flex>
+  );
+};
+
+const MemberItem = ({ avatar, first_name, last_name }) => {
+  return (
+    <Flex
+      className="p-2 ps-0 cursor-pointer"
+      justify="space-between"
+      align="center"
+    >
+      <Space>
+        <Avatar src={avatar} />
+        <Space>
+          <p className="text-slate-900 text-[13px] overflow-hidden whitespace-nowrap text-ellipsis max-w-[180px]">
+            {first_name + ' ' + last_name}
+          </p>
+        </Space>
+      </Space>
+      <Button type="dashed" icon={<FiUserPlus />} shape="round" />
+    </Flex>
+  );
+};
+
+export default ListMemberOfGroup;
