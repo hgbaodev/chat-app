08467fdd7b23cf4a1a10ffec18360ea2c6c5d2eb
Hoang Gia Bao <120194990+hgbaodev@users.noreply.github.com>
2024-03-24 01:24:07 +0700

    Update MesssageList.jsx

--- client/src/section/chats/MesssageList.jsx
+++ client/src/section/chats/MesssageList.jsx
@@ -1,90 +1,93 @@
-import { Space, Spin } from 'antd';
-import { useEffect } from 'react';
-import InfiniteScroll from 'react-infinite-scroll-component';
-import { RecallMessage, TextMessage } from '~/section/chats/MessageTypes';
-import { useDispatch, useSelector } from '~/store';
-import { getMessagesOfConversation, setPage } from '~/store/slices/chatSlice';
-import { MessageTypes } from '~/utils/enum';
+  import { Space, Spin } from 'antd';
+  import { useEffect } from 'react';
+  import InfiniteScroll from 'react-infinite-scroll-component';
+  import { RecallMessage, TextMessage } from '~/section/chats/MessageTypes';
+  import { useDispatch, useSelector } from '~/store';
+  import { getMessagesOfConversation, setPage } from '~/store/slices/chatSlice';
+  import { MessageTypes } from '~/utils/enum';
+  const MesssageList = () => {
+    const dispatch = useDispatch();
+    const { chat } = useSelector((state) => state.chat);
 
-const MesssageList = () => {
-  const dispatch = useDispatch();
-  const { chat } = useSelector((state) => state.chat);
+    useEffect(() => {
+      if (chat.currentConversation.id) {
+        dispatch(
+          getMessagesOfConversation({
+            conversation_id: chat.currentConversation.id,
+            page: chat.currentPage
+          })
+        );
+      }
+    }, [dispatch, chat.currentConversation, chat.currentPage]);
 
-  useEffect(() => {
-    if (chat.currentConversation.id) {
-      dispatch(
-        getMessagesOfConversation({
-          conversation_id: chat.currentConversation.id,
-          page: chat.currentPage
-        })
-      );
-    }
-  }, [dispatch, chat.currentConversation, chat.currentPage]);
+    const fetchMoreData = () => {
+      setTimeout(() => {
+        dispatch(setPage(chat.currentPage + 1));
+      }, 1000);
+    };
 
-  const fetchMoreData = () => {
-    if (chat.currentPage < chat.lastPage) {
-      dispatch(setPage(chat.currentPage + 1));
-    }
-  };
-  return (
-    <div
-      className="p-4 overflow-auto custom-scrollbar bg-gradient-to-b from-neutral-200 to-neutral-400"
-      style={{
-        height: 'calc(100vh - 120px)',
-        boxShadow:
-          '0px 1px 1px -1px rgba(0,0,0,.1), 0px -1px 1px -1px rgba(0,0,0,.1)',
-        display: 'flex',
-        flexDirection: 'column-reverse'
-      }}
-      id="scollable"
-    >
-      <InfiniteScroll
-        dataLength={chat.messages.length}
-        next={fetchMoreData}
-        className="flex flex-col-reverse overflow-y-auto"
-        inverse={true}
-        hasMore={chat.currentPage < chat.lastPage}
-        loader={<Spin className="py-2" />}
-        scrollableTarget="scollable"
+    return (
+      <div
+        className="p-4 overflow-auto custom-scrollbar bg-gradient-to-b from-neutral-200 to-neutral-400"
+        style={{
+          height: 'calc(100vh - 120px)',
+          boxShadow:
+            '0px 1px 1px -1px rgba(0,0,0,.1), 0px -1px 1px -1px rgba(0,0,0,.1)',
+          overflow: 'auto',
+          display: 'flex',
+          flexDirection: 'column-reverse'
+        }}
+        id="scrollableDiv"
       >
-        <Space direction="vertical">
-          {chat.messages.map((message, index) => {
-            let check = false;
-            if (index > 0) {
-              const currentMessageTime = new Date(message.created_at);
-              const prevMessageTime = new Date(
-                chat.messages[index - 1].created_at
-              );
-              const timeDiff = Math.abs(currentMessageTime - prevMessageTime);
-              const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60));
-              if (hoursDiff > 1) check = true;
-            }
-
-            switch (message.message_type) {
-              case MessageTypes.TEXT:
-                return (
-                  <TextMessage
-                    key={message.id}
-                    {...message}
-                    created={check ? message.created_at : null}
-                  />
+        <InfiniteScroll
+          dataLength={chat.messages.length}
+          className="flex flex-col-reverse overflow-y-auto"
+          next={fetchMoreData}
+          inverse={true}
+          hasMore={chat.currentPage < chat.lastPage}
+          loader={<Spin className="py-2" />}
+          scrollableTarget="scrollableDiv"
+        >
+          <Space direction="vertical">
+            {chat.messages.map((message, index) => {
+              let check = false;
+              if (index > 0) {
+                const currentMessageTime = new Date(message.created_at);
+                const prevMessageTime = new Date(
+                  chat.messages[index - 1].created_at
                 );
-              case MessageTypes.RECALL:
-                return (
-                  <RecallMessage
-                    key={message.id}
-                    {...message}
-                    created={check ? message.created_at : null}
-                  />
-                );
-              default:
-                return null;
-            }
-          })}
-        </Space>
-      </InfiniteScroll>
-    </div>
-  );
-};
+                const timeDiff = Math.abs(currentMessageTime - prevMessageTime);
+                const hoursDiff = Math.floor(timeDiff / (1000 * 60 * 60));
+                if (hoursDiff > 1) check = true;
+              }
 
-export default MesssageList;
+              switch (message.message_type) {
+                case MessageTypes.TEXT:
+                  return (
+                    <TextMessage
+                      key={message.id}
+                      {...message}
+                      created={check ? message.created_at : null}
+                    />
+                  );
+                case MessageTypes.RECALL:
+                  return (
+                    <RecallMessage
+                      key={message.id}
+                      {...message}
+                      created={check ? message.created_at : null}
+                    />
+                  );
+                default:
+                  return null;
+              }
+            })}
+          </Space>
+        </InfiniteScroll>
+      </div>
+    );
+  };
+
+  export default MesssageList;
+
+
