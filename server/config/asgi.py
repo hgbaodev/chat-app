3de21dff8daa64cdfdceb105f4b6b1a83d8311cd
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-05-04 16:20:44 +0700

    chore: Update ASGI configuration and Vercel deployment settings

--- server/config/asgi.py
+++ server/config/asgi.py
@@ -2,11 +2,12 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from channels_auth_token_middlewares.middleware import SimpleJWTAuthTokenMiddleware
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-from chat.routing import websocket_urlpatterns
 django_asgi_app = get_asgi_application()
+
+from chat.routing import websocket_urlpatterns
+from channels_auth_token_middlewares.middleware import SimpleJWTAuthTokenMiddleware
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
