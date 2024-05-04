2bf6e2f004aa7ed13e78410649375ddfa62b3a6a
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-05-04 22:02:57 +0700

    chore: Update ASGI configuration to use SimpleJWTAuthTokenMiddleware for WebSocket authentication

--- server/config/asgi.py
+++ server/config/asgi.py
@@ -1,17 +1,16 @@
 import os
-from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
+from chat.routing import websocket_urlpatterns
+from channels_auth_token_middlewares.middleware import SimpleJWTAuthTokenMiddleware
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 django_asgi_app = get_asgi_application()
 
-from chat.routing import websocket_urlpatterns
-
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket": URLRouter(websocket_urlpatterns)
+    "websocket": AllowedHostsOriginValidator(
+            SimpleJWTAuthTokenMiddleware(URLRouter(websocket_urlpatterns))
+        ),
 })
-
-app = application
