5c0696f0b40c5d9f5c1e933aa4354172cb4aee6e
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-05-04 19:01:09 +0700

    chore: Update build.sh to include collecting and clearing static files, and start the server after collecting static files

--- server/build.sh
+++ server/build.sh
@@ -6,7 +6,7 @@ python3.9 -m pip install -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
