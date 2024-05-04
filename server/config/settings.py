fde1789f7d9035e181395c649d4cee5dd4e99832
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-05-04 12:34:05 +0700

    chore: Update database configuration and settings

--- server/config/settings.py
+++ server/config/settings.py
@@ -90,19 +90,16 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {  
-    'default': {  
-        "ENGINE": "django.db.backends." + env('DB_CONNECTION'),
-        "NAME": env('DB_DATABASE'),
-        "USER": env('DB_USERNAME'),
-        "PASSWORD": env('DB_PASSWORD'),
-        "HOST": env('DB_HOST'),
-        "PORT": env('DB_PORT'),
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
         "OPTIONS": {
           'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
           'charset': 'utf8mb4',
         }
-    }  
+    }
 }
 
 DATABASES['default'] = dj_database_url.config()
