cade73436443255cefd55cfde639bc14c2272759
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-05-04 13:59:14 +0700

    chore: Update build script to remove redundant --noinput flag in manage.py migrate command

--- server/build.sh
+++ server/build.sh
@@ -6,7 +6,7 @@ python3.12 -m pip install -r requirements.txt
 
 echo "Make Migration..."
 python3.12 manage.py makemigrations --noinput
-python3.12 manage.py migrate --noinput
+python3.12 manage.py migrate
 
 echo "Collect Static..."
 python3.12 manage.py collectstatic --noinput --clear
