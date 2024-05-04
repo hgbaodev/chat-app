517075e19d34f0680604bafdc253b28aae0af5ea
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-05-04 15:03:30 +0700

    chore: Update socket endpoint URL in socketContext.jsx

--- client/src/contexts/socketContext.jsx
+++ client/src/contexts/socketContext.jsx
@@ -34,7 +34,7 @@ export const SocketProvider = ({ children }) => {
     let token = Cookies.get('token');
     if (!token) return;
     if (isAuthenticated) {
-      const endpoint = `wss://chatossd.app/ws/chat/${token}/`;
+      const endpoint = `wss://chatossd.vercel.app/ws/chat/${token}/`;
       var socket = new ReconnectingWebSocket(endpoint);
 
       socket.onopen = function (e) {
