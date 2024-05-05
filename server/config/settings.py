5d276c060c83ea8984cb0f39e1f6b5037c3a96b1
Trần Nhật Sinh <45101901+transinh085@users.noreply.github.com>
2024-05-05 12:18:42 +0700

    chore: Remove commented out 'corsheaders' app from INSTALLED_APPS in settings.py

--- server/config/settings.py
+++ server/config/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
-    'corsheaders',
+    # 'corsheaders',
     'authentication',
     'relationship',
     'chat',
